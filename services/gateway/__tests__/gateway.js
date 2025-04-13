b14d427384a464e4c91f408aa39fa1e660294c27
Lars van Herwijnen <lars.druten@gmail.com>
2025-04-13 15:24:29 +0200

    Update

--- services/gateway/__tests__/gateway.js
+++ services/gateway/__tests__/gateway.js
@@ -2,4 +2,7 @@ describe("Math Test", () => {
   it("should return 2 when adding 1 + 1", () => {
     expect(1 + 1).toBe(2);
   });
+  it("should return 3 when adding 1 + 2", () => {
+    expect(1 + 2).toBe(3);
+  });
 });
