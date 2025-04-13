7ffb937cf3177d7a31da1572278158d9f2827bb8
Lars van Herwijnen <lars.druten@gmail.com>
2025-04-13 16:12:57 +0200

    Remove coverage

--- services/service-b/eslint.config.js
+++ services/service-b/eslint.config.js
@@ -10,12 +10,6 @@ module.exports = [
         "no-console": "off",
         "semi": ["error", "always"],
         "quotes": ["error", "double"],
-        "coverage/coverage": [
-        "error",
-        {
-          threshold: 80, // Set your desired coverage threshold (e.g., 80%)
-        },
-      ],
       },
     },
   ];
